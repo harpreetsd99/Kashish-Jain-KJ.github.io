9d8f2751ea57ab75c999ddaaf91a72e86a4c16ea
Kashish Jain <jainkashish37@gmail.com>
2020-06-25 18:44:49 +0530

    nav-bar problem solved

--- Login/buyers_detail.js
+++ Login/buyers_detail.js
@@ -4,8 +4,6 @@ var card_number = 0;
 const user_db = db.collection('users');
 const products_db = db.collection('Products');
 
-
-
 products_db.get()
 .then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
@@ -14,7 +12,7 @@ products_db.get()
 
             snapshot.forEach((user_doc) => {
                 if(!user_doc.data().admin){
-                    console.log(doc.id)
+                    // console.log(doc.id)
                     products_db.doc(doc.id).collection("buyers_detail").doc(user_doc.data().email)
                     .set({
                         quantity : '',
@@ -28,42 +26,48 @@ products_db.get()
 })
 .catch(err => console.log(err))
 
-db.collectionGroup('buyers_detail').get()
-.then(function (querySnapshot) {
-    querySnapshot.forEach((buyers_doc) => {
-        console.log(buyers_doc.id, ' => ', buyers_doc.data());
 
+// getting product name from url
+const product_name = document.location.href.split('?')[1].split('=')[1];
+console.log(product_name);
+
+products_db.doc(product_name).collection("buyers_detail").get()
+.then((buyersSnapshot) => {
+    buyersSnapshot.forEach((buyers_doc) => {
+        console.log(buyers_doc.id)
+        
         user_db.get()
         .then((snapshot) => {
-
             snapshot.forEach((users_doc) => {
 
-                // if buyers_detail and users user_id are same
-                if(users_doc.data().admin == false) {                      
+                if(users_doc.data().admin == false && users_doc.data().email == buyers_doc.id) {                      
                     console.log(`${buyers_doc.data().price}, ${buyers_doc.data().quantity} =>
-                        ${users_doc.data().address}, ${users_doc.data().name}, ${users_doc.data().phone_no}`);                        
-                        // adding cards info to card-dynamic tag
-                        $(document).ready(function(){
-                            card_number = card_number + 1;
-                                $("#detailList").append(
-                                        `<li><div class="card">
-                                            <div class="card-content">
-                                                <span class="card-title" id="name">${users_doc.data().name}</span>
-                                                <p id="address">Address : ${users_doc.data().address}</p>
-                                                <p>Phone_no : ${users_doc.data().phone_no}</p>
-                                                <p>Quantity : ${buyers_doc.data().quantity}</p>
-                                                <p>Price : <span class="price">${buyers_doc.data().price}</span></p>
-                                            </div>
-                                        </div></li>`
+                    ${users_doc.data().address}, ${users_doc.data().name}, ${users_doc.data().phone_no}`);                        
+                    
+                    // adding cards info to card-dynamic tag
+                    $(document).ready(function(){
+                        card_number = card_number + 1;
+                        $("#detailList").append(
+                            `<li><div class="card">
+                                <div class="card-content">
+                                <span class="card-title" id="name">${users_doc.data().name}</span>
+                                <p id="address">Address : ${users_doc.data().address}</p>
+                                <p>Phone_no : ${users_doc.data().phone_no}</p>
+                                <p>Quantity : ${buyers_doc.data().quantity}</p>
+                                <p>Price : <span class="price">${buyers_doc.data().price}</span></p>
+                                </div>
+                            </div></li>`
                             );
                         });
                 }
             })
-        })        
-    })
+        })
+        .catch(err => console.log(err))        
+    })  
 })
 .catch(err => console.warn(err))
 
+ 
 function sort(list, key) {
     console.log("In sort function");
     $($(list).get().reverse()).each(function(outer) {
@@ -95,15 +99,3 @@ function sortReverse(list, key) {
     });
     console.log("Exiting  reverse sort function");
 }
-
-
-
-var dropdowns = document.querySelectorAll('.dropdown-trigger')
-for (var i = 0; i < dropdowns.length; i++){
-    M.Dropdown.init(dropdowns[i]);
-}
-
-
-
-const slide_menu = document.querySelectorAll(".sidenav");
-M.Sidenav.init(slide_menu,{});
