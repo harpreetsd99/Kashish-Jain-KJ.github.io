5edb4da323763d4e15c44b52f074b94d6f46f259
Kashish Jain <66963115+Kashish-Jain-KJ@users.noreply.github.com>
2020-07-06 12:47:57 +0530

    working on checkpoints

--- Customer-Side/productdetail.js
+++ Customer-Side/productdetail.js
@@ -15,6 +15,124 @@ docRef.get().then(function(doc) {
         document.getElementById("price").innerHTML = doc.data()['Product_Price'];
         var description = document.getElementById("description");
         document.getElementById("description").innerHTML = doc.data()['Product_Discription'];
+      
+        var x = 1;
+       
+        var count = 0;
+       
+        var a = "CheckPointQuantity" + x.toString();
+       
+        while(doc.data()[a]!=""){
+            count++;
+            if (count==5){
+                break;
+            }
+            x++;
+            a = "CheckPointQuantity" + x.toString();
+        }
+     
+      
+    //    document.getElementById("#numcheckpoint").innerHTML=count;
+
+      
+        console.log(count);
+        if(count>0){
+            $("checkpoint-content").show();
+            for(i=1;i<=count;i++){
+                $("#checkpoint-num").append(
+                    `<div class="bullet${i} bull left"><span id="num${i}">${i}</span><i class="material-icons">arrow_drop_down</i></div>`
+                );
+		    }
+		    $("#checkpoint").append($("#checkpoint-num").html());
+            for(i=1;i<=count;i++){
+                a = "CheckPointQuantity" + i.toString();
+                var b = "CheckPointPrice" + i.toString();
+                console.log(doc.data()[a]);
+                console.log(doc.data()[b]);
+
+			    $("#qty-checkpoint").append(
+				    `<sup id="label-qty${i}">
+	  			    <label>${ doc.data()[a]}</label>
+	  			    </sup>`
+			    );
+			    $("#price-checkpoint").append(
+				    `<sup  id="label-price${i}">
+				    <label>${ doc.data()[b]}&#8377;</label>
+				    </sup>`
+			    );
+            }
+            if(count==1){
+                $(".bullet1").css("left","100%");
+                $("#label-price1").css("left","95%");
+                $("#label-qty1").css("left","95%");
+            }
+            else if(count==2){
+                $(".bullet1").css("left","50%");
+                $(".bullet2").css("left","95%");
+                $("#label-price1").css("left","46.5%");
+                $("#label-qty1").css("left","46.5%");
+                $("#label-price2").css("left","90%");
+                $("#label-qty2").css("left","91%");
+            }
+            else if(count==3){
+                $(".bullet1").css("left","33.33%");
+                $(".bullet2").css("left","61.66%");
+                $(".bullet3").css("left","89.99%");
+                $("#label-price1").css("left","30.33%");
+                $("#label-qty1").css("left","30.33%");
+                $("#label-price2").css("left","58.66%");
+                $("#label-qty2").css("left","59.66%");
+                $("#label-price3").css("left","85.99%");
+                $("#label-qty3").css("left","87.99%");
+            }
+            else if(count==4){
+                $(".bullet1").css("left","25%");
+                $(".bullet2").css("left","45%");
+                $(".bullet3").css("left","65%");
+                $(".bullet4").css("left","85%");
+                $("#label-price1").css("left","22%");
+                $("#label-qty1").css("left","22%");
+                $("#label-price2").css("left","42%");
+                $("#label-qty2").css("left","43%");
+                $("#label-price3").css("left","62%");
+                $("#label-qty3").css("left","64%");
+                $("#label-price4").css("left","80.6%");
+                $("#label-qty4").css("left","84%");
+            }
+            else if(count==5){
+                $(".bullet1").css("left","20%");
+                $(".bullet2").css("left","35%");
+                $(".bullet3").css("left","50%");
+                $(".bullet4").css("left","65%");
+                $(".bullet5").css("left","80%");
+                $("#label-price1").css("left","17%");
+                $("#label-qty1").css("left","17%");
+                $("#label-price2").css("left","32%");
+                $("#label-qty2").css("left","33%");
+                $("#label-price3").css("left","47%");
+                $("#label-qty3").css("left","49%");
+                $("#label-price4").css("left","62%");
+                $("#label-qty4").css("left","65%");
+                $("#label-price5").css("left","77%");
+                $("#label-qty5").css("left","81%");
+            }
+        }else{
+            $("#checkpoint-details").hide();
+        }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
         db.collection("Products").where("Product_Category", "==",doc.data()['Product_Category']).get()
         .then(function(querySnapshot) {
         querySnapshot.forEach(function(docc) {
